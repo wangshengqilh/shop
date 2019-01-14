cfd182743beebb0b3c98f8b246f4f15cada4e4bc
wangshengqi <wangshengqi@lening.com>
2019-01-14 15:06:49 +0800

    添加

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         <ul>
             @foreach($list as $k=>$v)
-                <li>商品id：{{$v['goods_id']}}    --  商品名称：{{$v['goods_name']}}  -  商品价格：¥ {{$v['price'] / 100}}   --  加入时间：{{date('Y-m-d H:i:s',$v['add_time'])}}
+                <li>商品id:{{$v['goods_id']}}    --  商品名称:{{$v['goods_name']}}  -  商品价格:¥ {{$v['price'] / 100}}   --  加入时间:{{date('Y-m-d H:i:s',$v['add_time'])}}
                     <a href="/cart/del2/{{$v['goods_id']}}" class="del_goods">删除</a></li>
                     <a href="/pay/alipay/test" id="submit_order" class="btn btn-info "> 提交订单 </a>
             @endforeach
